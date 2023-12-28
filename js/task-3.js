31da696dfb7a8fd6ddb8fc7e22d180ce0756cbdb
oxfox1986 <145785269+oxfox1986@users.noreply.github.com>
2023-12-28 19:02:40 +0100

    go

---
+++ js/task-3.js
@@ -0,0 +1,18 @@
+"use strict";
+
+function filterArray(numbers, value) {
+  let filterValue = [];
+  for (let index = 0; index < numbers.length; index++) {
+    const element = numbers[index];
+    if (element > value) {
+      filterValue.push(element);
+    }
+  }
+  return filterValue;
+}
+
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
